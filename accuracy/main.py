c184a6fc5fcc186fd60814d8e979c07cec1196a3
Yu Ji <maple.jiyu@hotmail.com>
2018-04-24 22:13:15 +0000

    add parameters

--- accuracy/main.py
+++ accuracy/main.py
@@ -37,7 +37,24 @@ class BlocksparseModel(nn.Module):
         y = self.classifier(f)
         return y
 
+block_sizes = [
+    (0, 0, 16, 128), (0, 0, 16, 128), # conv1
+    (0, 0, 16, 128), (0, 0, 16, 128), # conv2
+    (0, 0, 16, 128), (0, 0, 16, 128), (0, 0, 16, 128), # conv3
+    (0, 0, 16, 128), (0, 0, 16, 128), (0, 0, 16, 128), # conv4
+    (0, 0, 16, 128), (0, 0, 16, 128), (0, 0, 16, 128), # conv5
+    (128, 128), (128, 128), # fc
+]
+
+pruning_rate = [
+    0.4, 0.5,
+    0.5, 0.5, 
+    0.5, 0.5, 0.5,
+    0.5, 0.5, 0.5,
+    0.5, 0.5, 0.5,
+    0.7, 0.7
+]
 
 model = models.vgg16(pretrained=True)
-sparse_model = BlocksparseModel(model)
+sparse_model = BlocksparseModel(model, block_sizes, pruning_rates)
 
