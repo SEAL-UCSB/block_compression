60c8836678c99a824ff6373afe31c3edddf75cf0
Yu Ji <maple.jiyu@hotmail.com>
2018-05-01 02:06:38 +0800

    fix run

--- accuracy/run.sh
+++ accuracy/run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 CONFIG=$1
-CUDA_VISIBLE_DEVICES=$2
-ITER=${3:-1}
+ITER=${2:-1}
 python main.py /disk/ImageNet2012/ "$CONFIG.json" --arch=vgg16_bn --lr=1e-3 --lr-epochs=5 --batch-size=64 --batch-iter=$ITER --prefix="vgg16_bn.$CONFIG"
