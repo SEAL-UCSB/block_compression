9dfb3ff6b7693a456a23bc58982b625311dd431f
Yu Ji <maple.jiyu@hotmail.com>
2018-05-06 08:14:21 +0800

    support arch for run

--- accuracy/run.sh
+++ accuracy/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 CONFIG=$1
+DEV=$2
+ARCH=$3
 ITER=${4:-1}
-CUDA_VISIBLE_DEVICES=$2 python main.py /disk/ImageNet2012/ "$CONFIG.json" --arch=$3 --lr=1e-3 --lr-epochs=5 --batch-size=64 --batch-iter=$ITER --prefix="vgg16_bn.$CONFIG" --print-freq=$((10 * $ITER))
+CUDA_VISIBLE_DEVICES=$DEV python main.py /disk/ImageNet2012/ "$CONFIG.json" --arch=$ARCH --lr=1e-3 --lr-epochs=5 --batch-size=64 --batch-iter=$ITER --prefix="$ARCH.$CONFIG" --print-freq=$((10 * $ITER))
