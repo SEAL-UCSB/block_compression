d0bcbd3e18069ff0677e4620c0338efa1252ce7f
Yu Ji <maple.jiyu@hotmail.com>
2018-05-06 08:12:00 +0800

    add alexnet for 8~64

--- accuracy/run.sh
+++ accuracy/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 CONFIG=$1
-ITER=${3:-1}
-CUDA_VISIBLE_DEVICES=$2 python main.py /disk/ImageNet2012/ "$CONFIG.json" --arch=vgg16 --lr=1e-3 --lr-epochs=5 --batch-size=64 --batch-iter=$ITER --prefix="vgg16_bn.$CONFIG" --print-freq=$((10 * $ITER))
+ITER=${4:-1}
+CUDA_VISIBLE_DEVICES=$2 python main.py /disk/ImageNet2012/ "$CONFIG.json" --arch=$3 --lr=1e-3 --lr-epochs=5 --batch-size=64 --batch-iter=$ITER --prefix="vgg16_bn.$CONFIG" --print-freq=$((10 * $ITER))
