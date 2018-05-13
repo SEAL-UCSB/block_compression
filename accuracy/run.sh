d8d919a2ba6cdff0abe2c9d681425a0fb4f7ead8
Yu Ji <maple.jiyu@hotmail.com>
2018-05-13 19:01:09 +0000

    update config

--- accuracy/run.sh
+++ accuracy/run.sh
@@ -4,4 +4,4 @@ CONFIG=$1
 DEV=$2
 ARCH=$3
 ITER=${4:-1}
-CUDA_VISIBLE_DEVICES=$DEV python main.py /disk/ImageNet2012/ "$CONFIG.json" --arch=$ARCH --lr=1e-3 --lr-epochs=5 --batch-size=64 --batch-iter=$ITER --prefix="$ARCH.$CONFIG.no_shuffle" --print-freq=$((10 * $ITER)) --no-shuffle
+CUDA_VISIBLE_DEVICES=$DEV python -W ignore main.py /disk/ImageNet2012/ "$CONFIG.json" --arch=$ARCH --lr=1e-3 --batch-size=64 --batch-iter=$ITER --prefix="$ARCH.$CONFIG" --print-freq=$((10 * $ITER))
