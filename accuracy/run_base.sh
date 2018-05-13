3c1d3aee3d3913b7495af24c735b654c1b2fb00b
Yu Ji <maple.jiyu@hotmail.com>
2018-05-14 06:20:25 +0800

    no shuffle

---
+++ accuracy/run_base.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+CONFIG=$1
+DEV=$2
+ARCH=$3
+ITER=${4:-1}
+CUDA_VISIBLE_DEVICES=$DEV python -W ignore main.py /disk/ImageNet2012/ "$CONFIG.json" --arch=$ARCH --lr=1e-3 --batch-size=64 --batch-iter=$ITER --prefix="$ARCH.$CONFIG.no-shuffle" --print-freq=$((10 * $ITER)) --no-shuffle
